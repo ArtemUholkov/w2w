3bdfa75ed4efb95e1119b795fd34538d8cdc9490
artem laptop <artem.ugolkov@ukr.net>
2024-12-30 12:35:51 +0200

    changes on hormone page

--- healthpage/js/script.js
+++ healthpage/js/script.js
@@ -1,37 +1,6 @@
-const burger = document.querySelector('#burger');
-const popup = document.querySelector('#popup');
-const popupSlide = document.querySelector('#popupSlide');
-const sliderItem = document.querySelectorAll('.our_requirements_content_item');
-const sliderPhoto = document.querySelector('.our_requirements_img');
-const body = document.body;
-
-burger.addEventListener('click', burgerHandler);
-popup.addEventListener('click', (e) => {
-  burgerHandler(e);
+const zoomImage = document.querySelector('.st_main_cover');
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  const zoomFactor = 1 + scrollPosition / 1000; // Adjust zoom sensitivity
+  zoomImage.style.transform = `scale(${zoomFactor})`;
 });
-
-sliderItem.forEach((e) => {
-  e.addEventListener('click', () => {
-    setImage(e, sliderItem);
-  });
-});
-
-function burgerHandler(e) {
-  if (popup.classList.contains('open')) {
-    popup.classList.add('close');
-    body.classList.remove('noscroll');
-    popupSlide.classList.add('slideout');
-
-    setTimeout(() => {
-      popup.classList.remove('close');
-      popup.classList.remove('open');
-      popupSlide.classList.remove('slideout');
-      popupSlide.classList.remove('slidein');
-    }, 300);
-  } else {
-    body.classList.add('noscroll');
-    popup.classList.add('open');
-    popupSlide.classList.add('slidein');
-  }
-  burger.classList.toggle('active');
-}
